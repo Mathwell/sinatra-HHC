cb6f807d4fc5e0bcb9f67de1f8bc4a0b1da462db
Olena Ageyeva <eageeva@hotmail.com>
2018-02-19 18:32:33 +0000

    CRUD for patients added

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -20,11 +20,32 @@ class PatientsController<ApplicationController
  end
 
  get '/patients/:slug/edit' do
-
+    @patient=Patient.find_by_slug(params[:slug])
+    erb :'patients/edit'
  end
 
- post '/patients/:slug/edit' do
+ get '/patients/:slug/delete' do
+    @nurse=Nurse.find_by(id: current_user.id)
+    @patient=Patient.find_by_slug(params[:slug])
+    if @nurse.patients.include?(@patient)
+      @nurse.patients.delete(@patient)
+      @patient.delete
+    end
+    erb :'nurses/show'
+ end
 
+ post '/patients/:slug' do
+    @patient=Patient.find_by_slug(params[:slug])
+    if @patient
+      @patient.first_name=params[:first_name]
+      @patient.last_name=params[:last_name]
+      @patient.ssn=params[:ssn]
+      @patient.insurance=params[:insurance]
+      @patient.save
+      erb :'/patients/show'
+    else
+      erb :'nurses/show'
+    end
  end
 
 end
